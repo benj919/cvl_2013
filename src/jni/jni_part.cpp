75d391706a57bfc7370c995f76f636b49855271d
benjamin <benjamin@ubuntu.(none)>
2013-11-03 21:02:57 +0100

    renamed relevant files and parts to natural feature tracking or nft

--- src/jni/jni_part.cpp
+++ src/jni/jni_part.cpp
@@ -5,47 +5,85 @@
 #include <vector>
 
 using namespace std;
-using namespace cv;
 
 cv::Mat descriptors_old;
 cv::Mat descriptors_new;
-vector<KeyPoint> v_old;
+vector<cv::KeyPoint> v_old;
+int selected_feature = 0;
+
+// initial object capture
+bool capture_frame = false;
+int capture_idx = 0;
+vector<vector<cv::KeyPoint> > initial_keypoints;
+vector<vector<cv::Mat> > initial_descriptors;
 
 extern "C" {
-JNIEXPORT void JNICALL Java_org_opencv_samples_tutorial2_Tutorial2Activity_FindFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba);
+JNIEXPORT void JNICALL Java_org_natural_feature_tracking_nftActivity_SetFeature(JNIEnv*, jobject, jint feature_idx);
 
-JNIEXPORT void JNICALL Java_org_opencv_samples_tutorial2_Tutorial2Activity_FindFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba)
+JNIEXPORT void JNICALL Java_org_natural_feature_tracking_nftActivity_FindFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba);
+
+JNIEXPORT void JNICALL Java_org_natural_feature_tracking_nftActivity_CaptureFrame(JNIEnv*, jobject, jint capture_idx);
+
+JNIEXPORT void JNICALL Java_org_natural_feature_tracking_nftActivity_FindFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba)
 {
-    Mat& mGr  = *(Mat*)addrGray;
-    Mat& mRgb = *(Mat*)addrRgba;
-    vector<KeyPoint> v_new;
-    std::vector<cv::DMatch> matches;
+    cv::Mat& mGr  = *(cv::Mat*)addrGray;
+    cv::Mat& mRgb = *(cv::Mat*)addrRgba;
+    vector<cv::KeyPoint> v_new;
+    std::vector<cv::DMatch> filteredMatches12, matches12, matches21;
 
     //FastFeatureDetector detector(50);
-    OrbFeatureDetector  detector(50);
-    OrbDescriptorExtractor extractor;
+    cv::OrbFeatureDetector  detector(50);
+    cv::OrbDescriptorExtractor extractor;
     cv::BFMatcher bf_matcher(cv::NORM_HAMMING, true);
 
     detector.detect(mGr, v_new);
-    extractor.compute(mRgb, v_new, descriptors_new);
+    detector.compute(mRgb, v_new, descriptors_new);
+
+    if (capture_frame){
+    	initial_keypoints.push_back(v_new);
+    	initial_descriptors.push_back(descriptors_new);
+    	capture_frame = false;
+    }
 
     if(descriptors_old.empty()){
     	descriptors_old = descriptors_new;
     	v_old = v_new;
     }
 
-    bf_matcher.match(descriptors_old, descriptors_new, matches);
+    bf_matcher.match( descriptors_old, descriptors_new, matches12 );
+    bf_matcher.match( descriptors_new, descriptors_old, matches21 );
 
-    for( unsigned int i = 0; i < matches.size(); i++ )
+    for( size_t i = 0; i < matches12.size(); i++ )
     {
-    	DMatch d = matches[i];
-        const KeyPoint& kp_new = v_new[d.trainIdx];
-        const KeyPoint& kp_old = v_old[d.queryIdx];
-        circle(mRgb, Point(kp_new.pt.x, kp_new.pt.y), 10, Scalar(0,0,255,100));
-        line(mRgb, Point(kp_old.pt.x, kp_old.pt.y), Point(kp_new.pt.x, kp_new.pt.y),Scalar(0,255,0,255));
+        cv::DMatch forward = matches12[i];
+        cv::DMatch backward = matches21[forward.trainIdx];
+        if( backward.trainIdx == forward.queryIdx
+        		and forward.distance < 0.5)
+            filteredMatches12.push_back( forward );
     }
+
+    for( unsigned int i = 0; i < filteredMatches12.size(); i++ )
+    {
+    	cv::DMatch d = filteredMatches12[i];
+        const cv::KeyPoint& kp_new = v_new[d.trainIdx];
+        const cv::KeyPoint& kp_old = v_old[d.queryIdx];
+        if(abs(kp_new.pt.x - kp_old.pt.x) < 20 and abs(kp_new.pt.y - kp_old.pt.y) < 20){
+        	cv::circle(mRgb, cv::Point(kp_new.pt.x, kp_new.pt.y), 10, cv::Scalar(0,0,255,100));
+        	cv::line(mRgb, cv::Point(kp_old.pt.x, kp_old.pt.y), cv::Point(kp_new.pt.x, kp_new.pt.y),cv::Scalar(0,255,0,255));
+        }
+    }
+
 
     v_old = v_new;
     descriptors_old = descriptors_new;
 }
+
+JNIEXPORT void JNICALL Java_org_natural_feature_tracking_nftActivity_SetFeature(JNIEnv*, jobject, jint feature_idx){
+	selected_feature = feature_idx;
+}
+
+JNIEXPORT void JNICALL Java_org_natural_feature_tracking_nftActivity_CaptureFrame(JNIEnv*, jobject, jint capture_idx){
+	capture_frame = true;
+}
+
 }
---
+++ src/src/org/nft/nftActivity.java
@@ -0,0 +1,190 @@
+package org.nft;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.imgproc.Imgproc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class nftActivity extends Activity implements CvCameraViewListener2 {
+    private static final String    TAG = "OCVSample::Activity";
+
+    private static final int       VIEW_MODE_FT_SELECTION     = 0;
+    private static final int       VIEW_MODE_GRAY     = 1;
+    private static final int       VIEW_MODE_CANNY    = 2;
+    private static final int       VIEW_MODE_FEATURES = 5;
+    
+    private int 				   selected_feature;
+    private static final int	   FEATURE_ORB = 0;
+    private static final int	   FEATURE_SURF = 1;
+    private static final int	   FEATURE_SIFT = 2;
+    private static final int	   FEATURE_STAR = 3;
+    private static final int	   FEATURE_MSER = 4;
+
+    private int                    mViewMode;
+    private Mat                    mRgba;
+    private Mat                    mIntermediateMat;
+    private Mat                    mGray;
+
+    private MenuItem               mItemFeatureSelection;
+    private MenuItem[] 			   mFeatureMenuItems;
+    private SubMenu 			   mFeatureMenu;
+    
+    private MenuItem               mItemPreviewGray;
+    private MenuItem               mItemPreviewCanny;
+    private MenuItem               mItemPreviewFeatures;
+
+    private CameraBridgeViewBase   mOpenCvCameraView;
+
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+
+                    // Load native library after(!) OpenCV initialization
+                    System.loadLibrary("mixed_sample");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public nftActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.tutorial2_surface_view);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial2_activity_surface_view);
+        mOpenCvCameraView.setMaxFrameSize(640, 480);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG, "called onCreateOptionsMenu");
+        //mItemFeatureSelection = menu.add("Features");
+        mFeatureMenu = menu.addSubMenu("Features");
+        mFeatureMenuItems = new MenuItem[5];
+        mFeatureMenuItems[0] = mFeatureMenu.add(2, FEATURE_ORB, Menu.NONE, "ORB");
+        mFeatureMenuItems[1] = mFeatureMenu.add(2, FEATURE_SURF, Menu.NONE, "SURF");
+        mFeatureMenuItems[2] = mFeatureMenu.add(2, FEATURE_SIFT, Menu.NONE, "SIFT");
+        mFeatureMenuItems[3] = mFeatureMenu.add(2, FEATURE_STAR, Menu.NONE, "STAR");
+        mFeatureMenuItems[4] = mFeatureMenu.add(2, FEATURE_MSER, Menu.NONE, "MSER");
+        
+        mItemPreviewGray = menu.add("Capture");
+        mItemPreviewCanny = menu.add("Canny");
+        mItemPreviewFeatures = menu.add("Find features");
+        return true;
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
+        mGray = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    public void onCameraViewStopped() {
+        mRgba.release();
+        mGray.release();
+        mIntermediateMat.release();
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        final int viewMode = mViewMode;
+        switch (viewMode) {
+        case VIEW_MODE_FT_SELECTION:
+            // input frame has RBGA format
+            mRgba = inputFrame.rgba();
+            break;
+        case VIEW_MODE_CANNY:
+            // input frame has gray scale format
+            mRgba = inputFrame.rgba();
+            Imgproc.Canny(inputFrame.gray(), mIntermediateMat, 80, 100);
+            Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+            break;
+        case VIEW_MODE_GRAY:
+            // input frame has gray scale format
+            CaptureFrame(0);
+            break;
+        case VIEW_MODE_FEATURES:
+            // input frame has RGBA format
+            mRgba = inputFrame.rgba();
+            mGray = inputFrame.gray();
+            FindFeatures(mGray.getNativeObjAddr(), mRgba.getNativeObjAddr());
+            break;
+        }
+
+        return mRgba;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
+
+        if (item.getGroupId() == 1){
+            selected_feature = item.getItemId();
+            SetFeature(selected_feature);
+            mViewMode = VIEW_MODE_FT_SELECTION;
+        } else if (item == mItemPreviewGray) {
+            mViewMode = VIEW_MODE_FEATURES;
+        } else if (item == mItemPreviewCanny) {
+            mViewMode = VIEW_MODE_CANNY;
+        } else if (item == mItemPreviewFeatures) {
+            mViewMode = VIEW_MODE_FEATURES;
+        }
+
+        return true;
+    }
+
+    public native void FindFeatures(long matAddrGr, long matAddrRgba);
+    public native void SetFeature(int feature_idx);
+    public native void CaptureFrame(int capture_idx);
+}
