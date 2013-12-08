45315ad2604d45022913f2c2679505ab2c398a50
benj <benj@gmx.net>
2013-12-08 18:14:31 +0100

    fixes

--- src/jni/tracking.cpp
+++ src/jni/tracking.cpp
@@ -11,10 +11,10 @@ using namespace cv;
 tracking::tracking(){
 	//kalman filter setup
 
-	kalman = cvCreateKalman( 6, 3, 0 );//state(x,y,z,detaX,detaY,detaZ)
-	process_noise = cvCreateMat( 6, 1, CV_32FC1 );
-	measurement = cvCreateMat( 3, 1, CV_32FC1 );//measurement(x,y,z)
-	rng = cvRNG(-1);
+	kalman = *cv::KalmanFilter( 6, 3, 0 );//state(x,y,z,deltaX,deltaY,deltaZ)
+	process_noise = cv::Mat( 6, 1, CV_32FC1 );
+	measurement = cv::Mat( 3, 1, CV_32FC1 );//measurement(x,y,z)
+	rng = cv::RNG(-1);
 
 
 };
@@ -26,45 +26,42 @@ tracking::~tracking(){
 };
 
 void tracking::initial_tracker(){
-	A={//transition matrix
-			1,0,0,0.5,0  ,0,
-			0,1,0,0  ,0.5,0,
-			0,0,1,0  ,0  ,0.5,
-			0,0,0,1  ,0  ,0,
-			0,0,0,0  ,1  ,0,
-			0,0,0,0  ,0  ,1
-	};
 
-	memcpy( kalman->transition_matrix->data.fl,A,sizeof(A));
-	cvSetIdentity(kalman->measurement_matrix,cvRealScalar(1) );
-	cvSetIdentity(kalman->process_noise_cov,cvRealScalar(1e-5));
-	cvSetIdentity(kalman->measurement_noise_cov,cvRealScalar(1e-1));
-	cvSetIdentity(kalman->error_cov_post,cvRealScalar(1));
+	kalman->transitionMatrix = *(cv::Mat_<float>(6,6) <<
+			1,0,0,1,0,0,
+			0,1,0,0,1,0,
+			0,0,1,0,0,1,
+			0,0,0,1,0,0,
+			0,0,0,0,1,0,
+			0,0,0,0,0,1);
+	cv::setIdentity(kalman->measurementMatrix,cv::Scalar(1) );
+	cv::setIdentity(kalman->processNoiseCov,cv::Scalar(1e-5));
+	cv::setIdentity(kalman->measurementNoiseCov,cv::Scalar(1e-1));
+	cv::setIdentity(kalman->errorCovPost,cv::Scalar(1));
 	//initialize post state of kalman filter at zero
-	cvRandArr(&rng,kalman->state_post,CV_RAND_UNI,cvRealScalar(0),cvRealScalar(0));
+	cv::randn(kalman->statePost, cv::Scalar(0), cv::Scalar(0.1));
 }
 
-Point3f tracking::pridict_tracker(){
-	const CvMat* prediction=cvKalmanPredict(kalman,0);
-	predict_pt=cvPoint((int)prediction->data.fl[0],(int)prediction->data.fl[1],(int)prediction->data.fl[2]);
+cv::Point3f tracking::predict_tracker(){
+	const cv::Mat* prediction=kalman->predict();
+	//cv::Point3f predict_pt=cv::Point3f(prediction->data.fl);
 	return predict_pt;
 }
 
 void tracking::update_measurement_tracker(Point3f realPosition){
-
 	measurement->data.fl[0]=(float)realPosition.x;
 	measurement->data.fl[1]=(float)realPosition.y;
 	measurement->data.fl[2]=(float)realPosition.z;
 }
 
 void tracking::update_tracker(){
-	cvKalmanCorrect( kalman, measurement );
+	kalman->correct(measurement);
 }
 
-Point3f tracking::track(Point3f realPosition){
+cv::Point3f tracking::track(cv::Point3f realPosition){
 //here still have same problem please try on your computer if your computer recognize it
-	CvPoint predict=tracking::pridict_tracker();
-	update_measurement_tracker(Point3f realPosition);
+	cv::Point3f predict=tracking::predict_tracker();
+	update_measurement_tracker(realPosition);
 	update_tracker();
 	return predict;
 
--- src/jni/tracking.hpp
+++ src/jni/tracking.hpp
@@ -1,13 +1,5 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/video/tracking.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/video/video.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <vector>
-#include <cmath>
-#include <opencv2/legacy/legacy.hpp>
-//#include <opencv2/core/matx.hpp>
 #include <iostream>
 
 #pragma once
@@ -17,21 +9,19 @@ class tracking{
 	// features
 public:
 
-	CvKalman* kalman;
-	CvMat* process_noise;
-	CvMat* measurement;
-	CvRNG rng;
-	//problem that it never recognize Matx66f, I am sure it is in documentation, but not work, try to include matx.hpp but not successful
-	Matx66f A;
-	Point3f predict_pt;
+	cv::KalmanFilter* kalman;
+	cv::Mat* process_noise;
+	cv::Mat* measurement;
+	cv::RNG rng;
+	cv::Point3f predict_pt;
 
 	tracking();
 	~tracking();
 
     void initial_tracker();
-    Point3f pridict_tracker();
-    void update_measurement_tracker(Point3f realPosition);
+    cv::Point3f predict_tracker();
+    void update_measurement_tracker(cv::Point3f realPosition);
     void update_tracker();
-    Point3f track(Point3f realPosition);
+    cv::Point3f track(cv::Point3f realPosition);
 
 };
